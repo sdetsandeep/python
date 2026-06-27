20f20d33c361b3ca370d6e6ff0de6ba3f84f6fde
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-06-28 00:16:42 +0530

    Create 5_variable.py

---
+++ 5_variable.py
@@ -0,0 +1,15 @@
+x = 5
+y = "John"
+print(" The valiue of x =",x)
+print("The value of y =",y)
+
+name="sandeep";
+print("The value of string variable name =",name)
+
+z= 20.36634
+print("The value of float variable z =",z);
+
+b1=True;
+b2=False;
+
+print ("The value of boolean variable b1=",b1 , " and value of b2=",b2)
