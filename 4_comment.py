f288a48e884ca3e546bc4b86876136dbfb2c50bd
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-06-28 00:12:40 +0530

    Create 4_comment.py

---
+++ 4_comment.py
@@ -0,0 +1,10 @@
+# comment 1
+print ("This is for comment single line use # sign before the content to be comment")
+
+"""
+one 
+two 
+third
+"""
+
+print (" use triple double quote for muultiline comments...")
