9584aaab80efb899a6b348be600de2aa66220e07
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-06-28 00:05:30 +0530

    updated and adeed 3rd program indentation
    updated and adeed 3rd program indentation

--- 2.SysInfo.py
+++ 2.SysInfo.py
@@ -2,15 +2,5 @@ import sys
 
 print(sys.version)
 
-if 5 > 2:
- print("Five is greater than two!")  
- print("Five is greater than 2!")
-print("Five is greater than twooo!")
 
-
-marks = 85
-
-if marks >= 33:
-    print("Pass")
-    print("Exam Finished")
 
