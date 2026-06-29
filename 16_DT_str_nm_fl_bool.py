e156bbbf124c77ad0adb15cdd83adf7d5532aff0
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-06-29 15:17:12 +0530

    Create 16_DT_str_nm_fl_bool.py

---
+++ 16_DT_str_nm_fl_bool.py
@@ -0,0 +1,42 @@
+# ==========================================
+# Python Data Types Example
+# ==========================================
+
+# -------------------------------
+# 1. Text Type : str
+# -------------------------------
+
+name = "Sandeep"
+
+print("String Value :", name)
+print("Data Type :", type(name))
+print()
+
+
+# -------------------------------
+# 2. Numeric Types
+# -------------------------------
+
+# Integer (Whole Number)
+age = 42
+
+print("Integer Value :", age)
+print("Data Type :", type(age))
+print()
+
+
+# Float (Decimal Number)
+percentage = 89.75
+
+# Bool (True or False)
+bool = False
+
+print("Float Value :", percentage)
+print("Data Type :", type(percentage))
+print()
+
+# Float (Decimal Number)
+
+print("Bool Value :", bool)
+print("Data Type :", type(bool))
+print()
