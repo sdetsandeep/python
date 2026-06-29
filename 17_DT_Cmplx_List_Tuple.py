2d7612764ea86ca94d3bbf96bc7b614ecdd2fcbf
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-06-29 15:41:59 +0530

    Create 17_DT_Cmplx_List_Tuple.py

---
+++ 17_DT_Cmplx_List_Tuple.py
@@ -0,0 +1,21 @@
+#complax number
+cmp = 5+3j;
+
+print("Complex Value :", cmp)
+print("Data Type :", type(cmp))
+print()
+
+# LIST  / SEQUENcE
+
+# List (Can be Changed)
+fruits=["Apple", "Mango", "Banana", "Kela", "SEB", "Aam"]
+print( "The list of fruit is ", fruits)
+print("The type of fruit is =", type(fruits))
+
+# Tuple (Cannot be Changed)
+colors = ("Red", "Green", "Blue")
+
+print("Tuple :", colors)
+print("Data Type :", type(colors))
+print()
+
