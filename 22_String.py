6b5f3c26e96c63d1bab02014b360a0c1e04e0183
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-06-30 00:42:27 +0530

    Create 22_String.py

---
+++ 22_String.py
@@ -0,0 +1,18 @@
+# Assign String to a Variable
+
+a = "Hello"
+print(a)
+
+# Quotes Inside Quotes
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+
+#Multiline Strings
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
