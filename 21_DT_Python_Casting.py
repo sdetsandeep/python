f025d85116445625fe2d8bf4fd5c24c0fd1b1784
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-06-30 00:36:27 +0530

    Create 21_DT_Python_Casting.py

---
+++ 21_DT_Python_Casting.py
@@ -0,0 +1,47 @@
+# ===========================================
+# Python Casting Example
+# int() , float() , str()
+# ===========================================
+
+# -------------------------------
+# Original Value
+# -------------------------------
+
+number = 25
+
+print("Original Value :", number)
+print("Original Data Type :", type(number))
+print()
+
+# -------------------------------
+# int()
+# -------------------------------
+
+x = int(25.89)
+
+print("Using int()")
+print("Value :", x)
+print("Data Type :", type(x))
+print()
+
+# -------------------------------
+# float()
+# -------------------------------
+
+y = float(number)
+
+print("Using float()")
+print("Value :", y)
+print("Data Type :", type(y))
+print()
+
+# -------------------------------
+# str()
+# -------------------------------
+
+z = str(number)
+
+print("Using str()")
+print("Value :", z)
+print("Data Type :", type(z))
+print()
