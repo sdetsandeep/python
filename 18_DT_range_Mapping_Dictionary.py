85277bcc117965fc933f7ad6ab84023628ced171
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-06-29 16:03:09 +0530

    Update 18_DT_range_Mapping_Dictionary.py

--- 18_DT_range_Mapping_Dictionary.py
+++ 18_DT_range_Mapping_Dictionary.py
@@ -6,3 +6,17 @@ print("The Numbers in given range is ...", list(numbers))
 print("Data Type :", type(numbers))
 print("===================================================================")
 
+#Dictionary  ( Key:Value)
+
+student={
+    "Name": "Sandeep",
+    "Age": 40,
+    "Job": "AI SDET",
+    "Company": "Monster.com"
+
+}
+
+print("The Detail of Student is ....", student)
+print("Data Type :", type(student))
+print()
+
