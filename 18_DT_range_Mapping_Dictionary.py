dcea13e4c49ae00127123c3e9cf4db95b5c25d6d
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-06-29 16:00:11 +0530

    Create 18_DT_range_Mapping_Dictionary.py

---
+++ 18_DT_range_Mapping_Dictionary.py
@@ -0,0 +1,8 @@
+
+#range 
+
+numbers = range(1,10);
+print("The Numbers in given range is ...", list(numbers))
+print("Data Type :", type(numbers))
+print("===================================================================")
+
