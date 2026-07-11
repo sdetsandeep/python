65dd54bf2a8d20a87e89aec823c2b40faccef914
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-07-12 00:50:16 +0530

    updated

--- app.py
+++ app.py
@@ -5,3 +5,19 @@ print("Step 1 : Library Loaded")
 client = chromadb.PersistentClient(path="mydb")
 
 print("Step 2 : Connected to Database")
+
+collection = client.get_or_create_collection(name="books")
+#collection = client.get_or_create_collection(name="books1")
+print("Step 3 : Collection Ready")
+
+# to see the collections in db 
+print(client.list_collections())
+
+collection.add(
+ids =["1"],
+documents= ["Python is Very easy programming Lanugage .."],
+   
+documents= ["java is  also a Very easy programming Lanugage . but toughest more than python."],
+)
+
+print("Step 4 : Document Added")
