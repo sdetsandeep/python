13fc132409768eed7939aec8bf07b72dca85919c
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-07-13 00:41:40 +0530

    updatte

--- app.py
+++ app.py
@@ -25,6 +25,23 @@ collection.add(
 documents= ["java is  also a Very easy programming Lanugage . but toughest more than python."],
 )
 
+collection.add(
+    ids=["3"],
+    documents=["LangChain is a framework."],
+    metadatas=[
+        {
+            "author":"Sandeep",
+            "subject":"AI"
+        }
+    ]
+)
 print("Step 4 : Document Added")
+#result =collection.get(ids=["3"]);
 
-print(collection.get())
+result =collection.query(
+    query_texts=["Best programming language for beginners"],
+    n_results=2
+)
+
+print(result)
+
